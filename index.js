09c412487783de78fd555b063914ae6ce48c0b00
soad-hm <saideh.s.hasheminejad@gmail.com>
2023-09-28 17:26:59 +0330

    compelete

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-var btn = document.querySelector('.btn')
-var show= document.querySelector('.how')
-var input= document.querySelector('input')
+let btn = document.querySelector('.btn')
+let show= document.querySelector('.how')
+let input= document.querySelector('input')
+let inputrevesed=document.querySelector('.reverse')
 
-
+function reverseString() { 
+    let str=input.value
+    return str.split('').reverse().join('');
+  }
 function showhow(){
     let number=input.value.length
     show.innerHTML=number
+    let nama=reverseString()
+    inputrevesed.innerHTML=nama
     setTimeout(function() {
        input.value=""
        show.innerHTML=""
+       inputrevesed.innerHTML=""
     }, 5000);
+
 }
-// btn.addEventListener('click', )
 btn.addEventListener("click",showhow)
+
+  
